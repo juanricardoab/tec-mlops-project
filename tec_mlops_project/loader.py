c820fe8121b2ad027ea6ae64a69142a0e88203f4
Raul Ayala <mac_roas@Rauls-MacBook-Pro.local>
2024-10-07 21:51:49 -0600

    Add the loader.py

---
+++ tec_mlops_project/loader.py
@@ -0,0 +1,10 @@
+import yaml 
+
+"""
+Load the data form the params.yaml file 
+"""
+
+with open("params.yaml") as conf_file:
+    config = yaml.safe_load(conf_file)
+
+print(config['data_load']['hour_csv'])
