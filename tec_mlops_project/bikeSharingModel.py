6e033a5940da46a1af5fdcd4bae2cac64478b6c7
chechopm <sepulidom@unal.edu.co>
2024-10-09 18:32:58 -0500

    Add dataExplorer functions

--- tec_mlops_project/bikeSharingModel.py
+++ tec_mlops_project/bikeSharingModel.py
@@ -8,6 +8,10 @@ from dataExplorer import DataExplorer
 class BikeSharingModel:
     def __init__(self, fileNumber):
         self.fileNumber = fileNumber
+        # defining continuous, categorical and dependent variable
+        self.continuous_variables = ['temp', 'atemp', 'hum', 'windspeed', 'casual', 'registered']
+        self.categorical_variables = ['season', 'yr', 'mnth', 'hr', 'holiday', 'weekday', 'workingday', 'weathersit']
+        self.dependent_variable = ['cnt']
         ##self.model_pipeline = Pipeline([
         ##    ('scaler', StandardScaler()),
         ##    ('classifier', LogisticRegression(max_iter=1000))
@@ -18,6 +22,13 @@ class BikeSharingModel:
         bike_sharing = fetch_ucirepo(id=self.fileNumber) 
         self.bike_sharing_df = bike_sharing.data.original
         DataExplorer.explore_data(self.bike_sharing_df)
+        self.data_cleaned = DataExplorer.changes_format_data(self.bike_sharing_df, self.categorical_variables)
+        DataExplorer.explore_data(self.data_cleaned)
+        DataExplorer.plot_histograms(self.data_cleaned)
+        DataExplorer.plot_distribution_graphs(self.data_cleaned)
+        DataExplorer.plot_correlation_matrix(self.data_cleaned)
+        DataExplorer.plot_correlation_graphs(self.data_cleaned, self.continuous_variables, self.dependent_variable, self.categorical_variables)
+        DataExplorer.plot_average_rent_over_time(self.data_cleaned)
         return self
 
     def preprocess_data(self):
