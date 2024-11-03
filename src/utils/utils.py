e3a31a16f07ff1b10a8f6245ce522f826ebd980e
juanricardoab <juanrtato@gmail.com>
2024-11-02 20:47:24 -0500

    :art: some functions to get several models were added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,7 +4,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
 
 def load_x_y_data(pathX, pathY):
     X = pd.read_csv("./data/processed/X.csv")
@@ -29,6 +32,17 @@ def split_data(X, y, test_size=0.2, random_state=42):
 def get_regresion_model(params={"fit_intercept":True}):
     return LinearRegression(**params)
 
+def get_svr_model(params={"kernel":"linear", "C":1.0}):
+    return SVR(**params)
+
+def get_knn_model(params={"n_neighbors":5}):
+    return KNeighborsRegressor(**params)
+
+def get_random_forest_model(params={"n_estimators":100, "max_depth":None}):
+    return RandomForestRegressor(**params)
+
+def get_decision_tree_model(params={"max_depth":None}):
+    return DecisionTreeRegressor(**params)
 
 def evaluate_model(model, X_train, X_test, y_train, y_test, y_pred):
     y_t = np.square(y_test)
